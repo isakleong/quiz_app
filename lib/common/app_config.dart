380e411fe74e356acd5bda6176efbce75bd0aeb2
dafageraldine <dafageraldine16@gmail.com>
2023-10-12 13:24:05 +0700

    created 1.1.0 using dev url (vendor)

--- lib/common/app_config.dart
+++ lib/common/app_config.dart
@@ -15,7 +15,7 @@ class AppConfig {
   static const String tesPublicUrl = "https://link.tirtakencana.com/SFATools/public/test";
 
   static const String baseUrlVendor = "https://mitra.tirtakencana.com";
-  static const String apiurlvendorpath = "/tangki-air-jerapah";
+  static const String apiurlvendorpath = "/tangki-air-jerapah-dev";
   String unknowerrorvendor = "100";
   String itemcodenotfoundvendor = "101";
   String dateinvalidvendor = "102";
